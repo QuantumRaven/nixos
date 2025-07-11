8b92f655738117dc7f5fb486341d864ce487728f
QuantumRaven <quantumncorvidae@proton.me>
2025-07-11 11:56:13 -0400

    Fix: Remove immich and nextcloud ports

--- hosts/andromeda/modules/firewall.nix
+++ hosts/andromeda/modules/firewall.nix
@@ -5,8 +5,6 @@
   networking.firewall = {
      allowedTCPPorts = [ 
      8000 # paperless-ngx
-     8080 # nextcloud - Google/OneDrive alterantive
-     2283 # immich - Google Photos alternative
      ];
 #     allowedUDPPorts = [ ... ];
   };
