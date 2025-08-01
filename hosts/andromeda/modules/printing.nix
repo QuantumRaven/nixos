77e67138630a73f5923cc70ce94ef8fcc7dd7a32
QuantumRaven <quantumncorvidae@proton.me>
2025-08-01 17:15:27 -0400

    Add: printing functionality

---
+++ hosts/andromeda/modules/printing.nix
@@ -0,0 +1,24 @@
+{ 
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+    ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+}
