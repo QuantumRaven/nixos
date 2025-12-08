94ceeb48eed6dc652c2f5348dd1ec94d707e2bc2
QuantumRaven <quantumncorvidae@proton.me>
2025-12-08 17:14:41 -0500

    Add: Cosmic Desktop Env
    Add cosmic desktop environment to NixOS

---
+++ hosts/andromeda/modules/cosmic.nix
@@ -0,0 +1,17 @@
+{ 
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-edit
+  ];
+
+}
