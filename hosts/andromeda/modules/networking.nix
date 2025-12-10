84ad810bb95c4e15a89241e193e9e5d022975198
QuantumRaven <quantumncorvidae@proton.me>
2025-12-09 21:25:30 -0500

    Remove: IP Address Unused
    Removed 192.168.1.200 from nameservers

--- hosts/andromeda/modules/networking.nix
+++ hosts/andromeda/modules/networking.nix
@@ -6,6 +6,6 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  networking.nameservers = ["192.168.1.200" "1.0.0.1" "1.1.1.1"];
+  networking.nameservers = ["1.0.0.1" "1.1.1.1"];
 
 }
