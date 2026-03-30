ccf4c7b5f42dc50c235f52dcfe931c6fa55257f0
QuantumRaven <quantumncorvidae@proton.me>
2026-03-30 11:32:39 -0400

    Refactor: Fix hostname issue

--- hosts/void/modules/networking.nix
+++ hosts/void/modules/networking.nix
@@ -1,8 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  networking.hostName = "nixos";
   networking = {
-    hostName = "void"; # Define your hostname.
     networkmanager.enable = true;  # Easiest to use and most distros use this by default.
     nameservers = ["8.8.8.8"];
+  };
 }
