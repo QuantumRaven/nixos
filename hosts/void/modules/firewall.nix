13c41cbd0f38f166f8d42f5fb4114420fad53a97
QuantumRaven <quantumncorvidae@proton.me>
2026-04-06 17:57:39 -0400

    Refactor: Remove unused ports

--- hosts/void/modules/firewall.nix
+++ hosts/void/modules/firewall.nix
@@ -4,25 +4,10 @@
 
   networking.firewall = {
      allowedTCPPorts = [
-      # Containers
-        3000 # Gitea
-      # Network File Share
-        111
-        2049
-        4000
-        4001
-        4002
-        20048
+
      ];
      allowedUDPPorts = [
-      # Network File Share
-        111
-        2049
-        4000
-        4001
-        4002
-        20048
-];
+     ];
   };
 
 }
