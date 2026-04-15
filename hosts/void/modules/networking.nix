e7d8127ef8747327c77bed4f029ee8180297e068
QuantumRaven <quantumncorvidae@proton.me>
2026-04-15 07:24:06 -0400

    Modify: Change system name to void from nixos

--- hosts/void/modules/networking.nix
+++ hosts/void/modules/networking.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  networking.hostName = "nixos";
+  networking.hostName = "void";
   networking = {
     networkmanager.enable = true;  # Easiest to use and most distros use this by default.
     nameservers = ["8.8.8.8"];
