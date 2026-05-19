e20e86ec609d62e97fffc04c28099d111415ec58
QuantumRaven <quantumncorvidae@proton.me>
2026-05-18 23:45:00 -0400

    Refactor: Switch from NetworkManager to systemd-networkd

--- hosts/void/modules/networking.nix
+++ hosts/void/modules/networking.nix
@@ -3,7 +3,26 @@
 {
   networking.hostName = "void";
   networking = {
-    networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-    nameservers = ["8.8.8.8"];
+    # Disable network manager
+    networkmanager.enable = false;
+    # Disable DHCP client to avoid conflicts with systemd-networkd
+    useDHCP = false;
+  };
+
+  # Enable systemd-networkd
+  systemd.network = {
+    enable = true;
+    networks."01-wan" = {
+      dns = ["8.8.8.8"];
+      matchConfig.Name = "eno1"; # Change to your actual interface name
+      address = [
+        "192.168.0.100/24"
+      ];
+      routes = [
+        {
+          Gateway = "192.168.0.1";
+        }
+      ];
+      };
   };
 }
