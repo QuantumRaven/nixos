77640f01c7f625db3b600a41916246582a5d4ac3
QuantumRaven <quantumncorvidae@proton.me>
2026-07-17 21:21:40 -0400

    Add allowedTCPPortRanges option
    Set up allowed port ranges from 8000 to 8100 for self-hosted services
    and applications.

--- hosts/void/modules/firewall.nix
+++ hosts/void/modules/firewall.nix
@@ -5,10 +5,14 @@
   networking.firewall = {
      allowedTCPPorts = [
        3456 # Vikunja
-       8000 # Python http module
        8080 # Nextcloud container
      ];
      allowedUDPPorts = [
+     ];
+     allowedTCPPortRanges = [
+       # For various self-hosted apps/services
+       from = 8000
+       to = 8100
      ];
      trustedInterfaces = [ "virbr0" ];
   };
