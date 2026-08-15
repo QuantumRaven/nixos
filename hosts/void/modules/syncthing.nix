b07fb3111e2cdfdf8cb548a2600cc578bbce8991
QuantumRaven <quantumncorvidae@proton.me>
2026-08-15 19:49:01 -0400

    Add syncthing

--- hosts/void/modules/syncthing.nix
+++ hosts/void/modules/syncthing.nix
@@ -9,7 +9,10 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    dataDir = "/home/corvidae/workspace/syncthing";
-    # guiAddress = "127.0.0.1:8384";
+    guiAddress = "0.0.0.0:8384";
+    settings.gui = {
+      user = "chloe";
+      password = "$2b$05$7Ji.Wn/J9r76uKKK0DHZaul3ZY.uzoNubhsIdjkb2X69TUOy6XZwS";
+    };
   };
 }
