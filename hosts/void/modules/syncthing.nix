0b7528c8aa25af4f03474e05e178b3d9bbf54aa5
QuantumRaven <quantumncorvidae@proton.me>
2026-09-09 22:46:29 -0400

    Refactor: Give gui it's own space

--- hosts/void/modules/syncthing.nix
+++ hosts/void/modules/syncthing.nix
@@ -10,9 +10,11 @@
     enable = true;
     openDefaultPorts = true;
     guiAddress = "0.0.0.0:8384";
-    settings.gui = {
-      user = "chloe";
-      password = "$2b$05$7Ji.Wn/J9r76uKKK0DHZaul3ZY.uzoNubhsIdjkb2X69TUOy6XZwS";
+    settings = {
+      gui = {
+        user = "chloe";
+        password = "$2b$05$7Ji.Wn/J9r76uKKK0DHZaul3ZY.uzoNubhsIdjkb2X69TUOy6XZwS";
+      };
     };
   };
 }
