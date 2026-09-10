9d639cd2e50a451b0937fbdc94a7a8336fa699b1
QuantumRaven <quantumncorvidae@proton.me>
2026-09-10 02:01:51 -0400

    Move folders to settings

--- hosts/void/modules/syncthing.nix
+++ hosts/void/modules/syncthing.nix
@@ -17,13 +17,13 @@
           id = "6HSE37A-2CDTMDV-GFB4Z5G-KDZWQPM-NCJZUAF-RVB5GBW-3E7FORB-JW4VDQW";
         };
       };
+      folders = {
+        "share" = {
+          id = "share";
+          path = "/var/lib/syncthing/share_all";
+          devices = [ "andromeda" ];
         };
-        folders = {
-          "share" = {
-            id = "share";
-            path = "/var/lib/syncthing/share_all";
-            devices = [ "andromeda" ];
-          };
+        };
       };
 };
 }
