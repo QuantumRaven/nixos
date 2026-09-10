ec73fcfac7bbad330a43ddced0fc889c2d7af87c
QuantumRaven <quantumncorvidae@proton.me>
2026-09-09 23:13:45 -0400

    Refactor: Remove credentials
    Removed credentials and set the IP to localhost only.

--- hosts/void/modules/syncthing.nix
+++ hosts/void/modules/syncthing.nix
@@ -9,12 +9,8 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    guiAddress = "0.0.0.0:8384";
+    guiAddress = "127.0.0.1:8384";
     settings = {
-      gui = {
-        user = "chloe";
-        password = "$2b$05$7Ji.Wn/J9r76uKKK0DHZaul3ZY.uzoNubhsIdjkb2X69TUOy6XZwS";
-      };
     };
   };
 }
