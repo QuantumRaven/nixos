c27367dd91aec6a95ede58f73f0883e3e0090676
QuantumRaven <quantumncorvidae@proton.me>
2026-09-10 00:03:56 -0400

    Add specific folder for syncthing betweeen devs

--- hosts/void/modules/syncthing.nix
+++ hosts/void/modules/syncthing.nix
@@ -15,6 +15,12 @@
         "Andromeda" = {
           id = "D2M4SH5-RYRMBVF-OIC67Q2-6WWX3QL-HNHZ6CX-I5VS3YT-MZYNXKP-5PB5YQL"
         };
+      folders = {
+          "syncthing" = {
+            path = "/home/corvidae/storage/syncthing";
+            devices = [ "Andromeda" ];
+          };
+        };
       };
     };
   };
