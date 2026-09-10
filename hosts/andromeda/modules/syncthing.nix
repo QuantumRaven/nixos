e9bc5b964c5b20a20e0255e5698143ba05e53f7d
QuantumRaven <quantumncorvidae@proton.me>
2026-09-10 01:13:13 -0400

    Remove folders section

--- hosts/andromeda/modules/syncthing.nix
+++ hosts/andromeda/modules/syncthing.nix
@@ -17,12 +17,6 @@
           id = "PIN7JW6-2Z6ZCGT-QHK2TSQ-TGPNXZ3-AI3O6M2-URZBNZI-BB3GXQT-B7PEJAW";
         };
       };
-      folders = {
-        "syncthing" = {
-          path = "/home/corvidae/syncthing";
-          devices = [ "void" ];
-        };
-      };
       };
     };
 }
