198baf121dbb67819de49a2484fad6a535539eac
QuantumRaven <quantumncorvidae@proton.me>
2026-09-10 00:19:52 -0400

    Remove extra space

--- hosts/void/modules/syncthing.nix
+++ hosts/void/modules/syncthing.nix
@@ -15,6 +15,7 @@
         "Andromeda" = {
           id = "D2M4SH5-RYRMBVF-OIC67Q2-6WWX3QL-HNHZ6CX-I5VS3YT-MZYNXKP-5PB5YQL";
         };
+      };
       folders = {
           "syncthing" = {
             path = "/home/corvidae/storage/syncthing";
@@ -23,5 +24,4 @@
         };
       };
     };
-  };
 }
