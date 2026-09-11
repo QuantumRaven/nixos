bb9d5f1e8136b7ca27213b8a8803a7f4dcb256f9
QuantumRaven <quantumncorvidae@proton.me>
2026-09-11 00:39:16 -0400

    Add imports for user components

---
+++ hosts/void/modules/users/corvidae/default.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, lib, ... }:
+
+let
+  # Import each package file and extract its list
+  browserPackages = import ./packages/browsers.nix { inherit pkgs; };
+  devPackages     = import ./packages/dev.nix { inherit pkgs; };
+  mediaPackages   = import ./packages/media.nix { inherit pkgs; };
+  appPackages     = import ./packages/apps.nix { inherit pkgs; };
+in
+{
+  users.users.corvidae = {
+    isNormalUser = true;
+    description = "corvidae";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    shell = pkgs.bash;
+
+    # Concatenate all modular package lists together
+    packages =
+      browserPackages.home.packages ++
+      devPackages.home.packages ++
+      mediaPackages.home.packages ++
+      appPackages.home.packages;
+  };
+}
