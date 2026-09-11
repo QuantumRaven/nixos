5e7cf6a0917a738f213477840a597fdab647887a
QuantumRaven <quantumncorvidae@proton.me>
2026-09-11 02:10:26 -0400

    fix: opengl to graphics

--- hosts/void/modules/core/opengl.nix
+++ hosts/void/modules/core/opengl.nix
@@ -3,9 +3,8 @@
 {
 # OpenGL Driver Setup
   # Enable OpenGL
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 }
