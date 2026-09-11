c6a463158363914a5ba868572818c77d19167c54
QuantumRaven <quantumncorvidae@proton.me>
2026-09-11 02:05:17 -0400

    fix: add missed nix confs

--- hosts/void/modules/core/default.nix
+++ hosts/void/modules/core/default.nix
@@ -1,10 +1,13 @@
 {
   imports = [
+    ./amd.nix
     ./bootloader.nix
+    ./firewall.nix
     ./networking.nix
+    ./opengl.nix
+    ./openssh.nix
     ./storage.nix
     ./time.nix
     ./virtualization.nix
-    ./xwayland.nix
   ];
 }
