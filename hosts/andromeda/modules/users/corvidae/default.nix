1a5490ea36e783170baf0089c1c4a59c8366cf80
QuantumRaven <quantumncorvidae@proton.me>
2026-09-12 20:58:34 -0400

    fix: add in portion to default.nix

--- hosts/andromeda/modules/users/corvidae/default.nix
+++ hosts/andromeda/modules/users/corvidae/default.nix
@@ -11,3 +11,19 @@ let
   browserPackages = import ./packages/browsers.nix { inherit pkgs; };
   devPackages = import ./packages/dev.nix { inherit pkgs; };
   mediaPackages = import ./packages/media.nix { inherit pkgs; };
+in
+  {
+    users.users.corvidae = {
+      isNormalUser = true;
+      description = "corvidae";
+      extraGroups = [ "networkmanager" "wheel"];
+      shell = pkgs.bash;
+
+      # Concatenate all modular package lists together
+      packages =
+        browserPackages.home.packages ++
+        devPackages.home.packages ++
+        mediaPackages.home.packages ++
+        appPackages.home.packages;
+    };
+  }
