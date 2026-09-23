62ffe45b1a9f3b5f333cdea2ef990c7c431f45b4
QuantumRaven <quantumncorvidae@proton.me>
2026-09-23 13:45:21 -0400

    add learningPackages

--- hosts/void/modules/users/corvidae/default.nix
+++ hosts/void/modules/users/corvidae/default.nix
@@ -2,10 +2,12 @@
 
 let
   # Import each package file and extract its list
+  appPackages = import ./packages/apps.nix { inherit pkgs; };
   browserPackages = import ./packages/browsers.nix { inherit pkgs; };
-  devPackages     = import ./packages/dev.nix { inherit pkgs; };
-  mediaPackages   = import ./packages/media.nix { inherit pkgs; };
-  appPackages     = import ./packages/apps.nix { inherit pkgs; };
+  devPackages = import ./packages/dev.nix { inherit pkgs; };
+  learningPackages = import ./packages/learning.nix { inherit pkgs; };
+  mediaPackages = import ./packages/media.nix { inherit pkgs; };
+
 in
 {
   users.users.corvidae = {
@@ -16,9 +18,10 @@ in
 
     # Concatenate all modular package lists together
     packages =
+      appPackages.home.packages ++
       browserPackages.home.packages ++
       devPackages.home.packages ++
-      mediaPackages.home.packages ++
-      appPackages.home.packages;
+      learningPackages.home.packages ++
+      mediaPackages.home.packages;
   };
 }
