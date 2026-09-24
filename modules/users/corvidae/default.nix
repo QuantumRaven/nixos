bbe9ea68d8fbf06280b5bd16be64ee714c769315
QuantumRaven <quantumncorvidae@proton.me>
2026-09-24 14:36:33 -0400

    fix: add addt'l ..

--- modules/users/corvidae/default.nix
+++ modules/users/corvidae/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let
-  secretPassword = import ../../secrets/password.nix;
+  secretPassword = import ../../../secrets/password.nix;
   # Import each package file and extract its list
   appPackages = import ./packages/apps.nix { inherit pkgs; };
   browserPackages = import ./packages/browsers.nix { inherit pkgs; };
