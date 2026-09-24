83439879b9a5dee1f878ce6f5cb6408e905434ab
QuantumRaven <quantumncorvidae@proton.me>
2026-09-24 09:00:04 -0400

    refactor: move git, neovim, starship

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -3,9 +3,12 @@
     ./amd.nix
     ./bootloader.nix
     ./firewall.nix
+    ./git.nix
+    ./neovim.nix
     ./networking.nix
     ./opengl.nix
     ./openssh.nix
+    ./starship.nix
     ./storage.nix
     ./time.nix
     ./virtualization.nix
